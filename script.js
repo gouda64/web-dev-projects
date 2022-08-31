4b9ac64fcff76adb7680a0a9ed7993ce78c0b45d
Erica Dong <erica3280cell@gmail.com>
2022-08-30 21:13:32 -0400

    Implement single round playing

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function getComputerChoice() {
     const rand = Math.floor(Math.random()*3);
     if (rand === 0) {
-        return "Rock";
+        return "rock";
     }
     else if (rand === 1) {
-        return "Paper";
+        return "paper";
     }
     else if (rand === 2) {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return 0;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"
+            || playerSelection === "scissors" && computerSelection === "paper"
+            || playerSelection === "paper" && computerSelection === "rock") {
+        return 1;  
+    }
+    else {
+        return -1;
     }
 }

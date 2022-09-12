661e57419c24226a7517e281a17e57706e7fa54d
Erica Dong <erica3280cell@gmail.com>
2022-09-12 12:16:10 -0400

    Fix reversed messages

--- script.js
+++ script.js
@@ -36,12 +36,12 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === "rock" && computerSelection === "scissors"
             || playerSelection === "scissors" && computerSelection === "paper"
             || playerSelection === "paper" && computerSelection === "rock") {
-        summText.textContent = `Since ${computerSelection} beats ${playerSelection}, you lost this round 😢`;
+        summText.textContent = `Since ${playerSelection} beats ${computerSelection}, you lost this round 😢`;
         computerScore++;
         robotScoreText.textContent = computerScore;  
     } 
     else {
-        summText.textContent = `Since ${playerSelection} beats ${computerSelection}, you won this round!`;
+        summText.textContent = `Since ${computerSelection} beats ${playerSelection}, you won this round!`;
         playerScore++;
         humanScoreText.textContent = playerScore;
     }

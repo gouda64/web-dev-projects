447c167e2895df0d3ae8c76f06b1d640c1462940
Erica Dong <erica3280cell@gmail.com>
2022-09-18 19:44:06 -0400

    Add hover drawing

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let gridSize = 16;
 
 let display = document.querySelector(".display");
-let slider = document.querySelector(".slider");
-slider.oninput = (event) => {
-    gridSize = event.target.value;
-    event.target.parentElement.firstChild.textContent = `${gridSize} x ${gridSize}`;
-    setGrid(gridSize);
-};
+
+function addListeners() {
+    display.childNodes.forEach((pixel) => {
+        pixel.onmouseover = (event) => {
+            event.target.style.backgroundColor = "#84827d";
+        };
+    });
+}
 
 function setGrid(gridSize) {
     display.style.gridTemplate = `repeat(${gridSize}, 1fr) / repeat(${gridSize}, 1fr)`;
@@ -19,5 +21,14 @@ function setGrid(gridSize) {
         div.classList.add("display-child");
         display.appendChild(div);
     }
+
+    addListeners();
 }
 setGrid(gridSize);
+
+let slider = document.querySelector(".slider");
+slider.oninput = (event) => {
+    gridSize = event.target.value;
+    event.target.parentElement.firstChild.textContent = `${gridSize} x ${gridSize}`;
+    setGrid(gridSize);
+};

432e5fbec3900010b6f7e995f33240f3c7ca86ff
Erica Dong <erica3280cell@gmail.com>
2022-09-19 12:49:27 -0400

    Add mode switch

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let gridSize = 16;
+let mode = 0; //0 is normal, 1 is click
 
 let display = document.querySelector(".display");
 
@@ -31,4 +32,23 @@ slider.oninput = (event) => {
     gridSize = event.target.value;
     event.target.parentElement.firstChild.textContent = `${gridSize} x ${gridSize}`;
     setGrid(gridSize);
+};
+
+let modeSwitch = document.querySelector(".switch");
+let switchThumb = document.querySelector(".switch .switch-thumb");
+
+modeSwitch.onclick = (event) => { 
+    //using event.target causes some issues with child
+    if (mode === 0) {
+        modeSwitch.style.backgroundColor = "#545863";
+        switchThumb.style.backgroundColor = "#f3e6d9";
+        switchThumb.style.transform = "translateX(28px)";
+        mode = 1;
+    }
+    else {
+        modeSwitch.style.backgroundColor = null;
+        switchThumb.style.backgroundColor = "#545863";
+        switchThumb.style.transform = null;
+        mode = 0;
+    }
 };

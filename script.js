ed51302081daa518f085f3476e9b956030049b16
Erica Dong <erica3280cell@gmail.com>
2022-09-20 14:39:32 -0400

    Add mode buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let gridSize = 16;
-let mode = 0; //0 is normal, 1 is click
+let drawMode = 0; //0 is normal, 1 is click
+let inMode = 0; //0 is normal, 1 is rainbow, 2 is transparent, 3 is erase
 let color = "#84827d";
 
 let display = document.querySelector(".display");
@@ -7,7 +8,7 @@ let display = document.querySelector(".display");
 function addListeners() {
     display.childNodes.forEach((pixel) => {
         pixel.onmouseover = (event) => {
-            if (mode === 0 || event.buttons) {
+            if (drawMode === 0 || event.buttons) {
                 event.target.style.backgroundColor = color;
             }
         };
@@ -45,17 +46,17 @@ let switchThumb = document.querySelector(".switch .switch-thumb");
 
 modeSwitch.onclick = (event) => {
     //using event.target causes some issues with child
-    if (mode === 0) {
+    if (drawMode === 0) {
         modeSwitch.style.backgroundColor = "#545863";
         switchThumb.style.backgroundColor = "#f3e6d9";
         switchThumb.style.transform = "translateX(28px)";
-        mode = 1;
+        drawMode = 1;
     }
     else {
         modeSwitch.style.backgroundColor = null;
         switchThumb.style.backgroundColor = "#545863";
         switchThumb.style.transform = null;
-        mode = 0;
+        drawMode = 0;
     }
 };
 
@@ -65,3 +66,15 @@ colorInput.oninput = (event) => {
     color = event.target.value;
     colorBox.style.backgroundColor = color;
 };
+
+document.querySelectorAll(".sidebar-option.mode").forEach((mode) => {
+    mode.onclick = (event) => {
+        let old = document.querySelector(".sidebar-option.mode[data-mode='" + inMode + "']");
+        old.style.backgroundColor = "#f3e6d9";
+        old.style.color = "#545863";
+
+        inMode = event.target.dataset.mode;
+        event.target.style.backgroundColor = "#545863";
+        event.target.style.color = "#f3e6d9";
+    };
+});

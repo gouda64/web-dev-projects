ba2351d46bfa38f010b21ba9f774b53131abbfd4
Erica Dong <erica3280cell@gmail.com>
2022-10-25 21:27:53 -0400

    Add keyboard functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const operatorSymbols = new Set(["^","%","/","*","-","+"]);
+
 const displayText = document.querySelector(".top .display span");
 let currNumber = displayText.textContent; //string
 let lastNumber = "";
@@ -14,39 +16,41 @@ numbers.forEach((number) => number.onclick = (event) => {
 });
 
 operators.forEach((operator) => operator.onclick = (event) => {
-    switch(event.target.dataset.symbol) {
-        case ".":
-            currNumber = currNumber + ".";
-            break;
-        case "delete":
-            currNumber = currNumber.slice(0, currNumber.length-1);
-            if (currNumber === "") currNumber = "0";
-            break;
-        case "clear":
-            currNumber = "0";
-            lastNumber = ""; currOperation = "";
-            break;
-        case "posneg":
-            if (currNumber.slice(0, 1) === "-") 
-                currNumber = currNumber.slice(1);
-            else currNumber = "-" + currNumber;
-            break;
-        case "=":
-            if (currOperation !== "")
-                currNumber = "" + operate(lastNumber, currNumber, currOperation);
-            lastNumber = "";
-            currOperation = "";
-            break;
-        default:
-            if (currOperation !== "")
-                currNumber = "" + operate(lastNumber, currNumber, currOperation);
-            lastNumber = currNumber;
-            currNumber = "0";
-            currOperation = event.target.dataset.symbol;
-            displayText.textContent = lastNumber;
-            return;
+    if (operatorSymbols.has(event.target.dataset.symbol)) {
+        if (currOperation !== "")
+            currNumber = "" + operate(lastNumber, currNumber, currOperation);
+        lastNumber = currNumber;
+        currNumber = "0";
+        currOperation = event.target.dataset.symbol;
+        displayText.textContent = lastNumber;
     }
-    displayText.textContent = currNumber;
+    else {
+        switch(event.target.dataset.symbol) {
+            case ".":
+                if (!currNumber.includes(".")) currNumber = currNumber + ".";
+                break;
+            case "delete":
+                currNumber = currNumber.slice(0, currNumber.length-1);
+                if (currNumber === "") currNumber = "0";
+                break;
+            case "clear":
+                currNumber = "0";
+                lastNumber = ""; currOperation = "";
+                break;
+            case "posneg":
+                if (currNumber.slice(0, 1) === "-") 
+                    currNumber = currNumber.slice(1);
+                else currNumber = "-" + currNumber;
+                break;
+            case "=":
+                if (currOperation !== "")
+                    currNumber = "" + operate(lastNumber, currNumber, currOperation);
+                lastNumber = "";
+                currOperation = "";
+                break;
+        }
+        displayText.textContent = currNumber;
+    }
 });
 
 function operate(a, b, operator) {
@@ -62,3 +66,54 @@ function operate(a, b, operator) {
     }
     return Math.round(fin*100000)/100000;
 }
+
+window.onkeydown = (event) => {
+    if (!isNaN(event.key)) {
+        if (currNumber === "0" || currNumber === "NaN" || currNumber === "Infinity") 
+            currNumber = event.key;
+        else currNumber = currNumber + event.key;
+        displayText.textContent = currNumber;
+    }
+    else if (operatorSymbols.has(event.key)) {
+        if (currOperation !== "")
+            currNumber = "" + operate(lastNumber, currNumber, currOperation);
+        lastNumber = currNumber;
+        currNumber = "0";
+        currOperation = event.key;
+        displayText.textContent = lastNumber;
+    }
+    else {
+        switch (event.key) {
+            case ".":
+                if (!currNumber.includes(".")) currNumber = currNumber + ".";
+                displayText.textContent = currNumber;
+                break;
+            case "Backspace":
+                currNumber = currNumber.slice(0, currNumber.length-1);
+                if (currNumber === "") currNumber = "0";
+                displayText.textContent = currNumber;
+                break;
+            case "c":
+            case "C":
+                currNumber = "0";
+                lastNumber = ""; currOperation = "";
+                displayText.textContent = currNumber;
+                break;
+            case "p":
+            case "P":
+                if (currNumber.slice(0, 1) === "-") 
+                    currNumber = currNumber.slice(1);
+                else currNumber = "-" + currNumber;
+                displayText.textContent = currNumber;
+                break;
+            case "Enter":
+            case "=":
+                if (currOperation !== "")
+                    currNumber = "" + operate(lastNumber, currNumber, currOperation);
+                lastNumber = "";
+                currOperation = "";
+                displayText.textContent = currNumber;
+                break;
+        }
+    }      
+};
